19c64da100c2e52d3aba532c5c77f3e448c4ffe3
Kamyak <163840181+KamyaK-1@users.noreply.github.com>
2025-05-01 17:04:47 +0530

    Add files via upload

--- simSkillPath.py
+++ simSkillPath.py
@@ -1,38 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
-T = 10
+T = 100
 time = np.arange(T)
 
 skill_values = {1: 1, 2: 2, 3: 3}
 
-transition_probs = {
-    1: 0.4,
-    2: 0.3,
-    3: 0.3 
-}
+transition_probs = [
+    {},
+    {1: 0.6, 2: 0.3, 3: 0.1},
+    {1: 0.1, 2: 0.6, 3: 0.3},
+    {1: 0.1, 2: 0.3, 3: 0.6},
+]
 
 def simulate_skill_path(promo_probs, T=10, seed=None):
     if seed is not None:
         np.random.seed(seed)
-    skills = [1]  # start at Basic
+    skill_values = [1]  # start at Basic
     for t in range(1, T):
-        current = skills[-1]
-        if current < 3 and np.random.rand() < promo_probs[current]:
-            skills.append(current + 1)
-        else:
-            skills.append(current)
-    return skills
+        current = skill_values[-1]
+        skills   = list(transition_probs[current].keys())
+        weights  = list(transition_probs[current].values())
+        new_level = random.choices(skills, weights=weights, k=1)[0]
+        skill_values.append(new_level)
+    return skill_values
 
 skill_path = simulate_skill_path(transition_probs, T, seed=42)
 
 discount_factors = {
-    "Short-term": 0.5,
-    "Medium-term": 0.8,
+    "Short-term": 0.8,
+    "Medium-term": 0.9,
     "Long-term": 0.97
 }
 
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(12, 6))
 for label, delta in discount_factors.items():
     discounted_values = [delta**t * skill_values[skill] for t, skill in enumerate(skill_path)]
     cumulative_values = np.cumsum(discounted_values)
